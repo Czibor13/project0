df129db2138dd744d61e9b0547e37aed1e49a0da
Ryne Czibor <65476337+Czibor13@users.noreply.github.com>
2021-11-24 13:00:24 -0600

    Added Reset button and refactored code.
    Started storing results of getElementByID in constants if it is needed more than once.
    Added ability to reset the game.
    Started working on CatGrass

--- cat.js
+++ cat.js
@@ -2,40 +2,72 @@ if (!localStorage.getItem('cats')) {
     localStorage.setItem('cats', 0);
 }
 
+if (!localStorage.getItem('catGrass')){
+    localStorage.setItem('catGrass', 0);
+}
+
+let cats = localStorage.getItem('cats');
+let catGrass = localStorage.getItem('catGrass');
+
 function count() {
-    let cats = localStorage.getItem('cats');
     cats++;
-    document.getElementById('displayCats').innerHTML = cats;
+    displayCats.innerHTML = cats;
     localStorage.setItem('cats', cats);
 }
 
-function about() {
-    document.querySelectorAll('div').forEach(div => {
-        div.style.display = 'none';
-    });
-
-    document.getElementById('gameNav').classList.remove('active')
-
-
-    document.getElementById('about').style.display = 'block';
-    document.getElementById('aboutNav').classList.add('active');
-}
-
-function game() {
-    document.querySelectorAll('div').forEach(div => {
-        div.style.display = 'none';
-    });
-
-    document.getElementById('aboutNav').classList.remove('active')
-
-    document.getElementById('game').style.display = 'block';
-    document.getElementById('gameNav').classList.add('active');
-}
-
 document.addEventListener('DOMContentLoaded', function() {
+    const aboutNav = document.getElementById('aboutNav');
+    const gameNav = document.getElementById('gameNav');
+    const resetNav = document.getElementById('resetNav');
+    const gameDiv = document.getElementById('game');
+    const aboutDiv = document.getElementById('about');
+    const resetDiv = document.getElementById('reset');
+    const displayCats = document.getElementById('displayCats');
 
-    document.getElementById('displayCats').innerHTML = localStorage.getItem('cats');
+    function hide(){
+        gameNav.classList.remove('active');
+        aboutNav.classList.remove('active');
+        resetNav.classList.remove('active');
+        gameDiv.style.display = 'none';
+        aboutDiv.style.display = 'none';
+        resetDiv.style.display = 'none';
+    }
+    
+    function about() {
+        hide();
+    
+        aboutDiv.style.display = 'block';
+        aboutNav.classList.add('active');
+    }
+    
+    function game() {
+        hide();
+    
+        gameDiv.style.display = 'block';
+        gameNav.classList.add('active');
+    }
+
+    function reset() {
+        hide();
+
+        resetDiv.style.display = 'block';
+        resetNav.classList.add('active');
+    }
+
+    function resetGame() {
+        cats = 0;
+        catGrass = 0;
+        localStorage.setItem('cats', cats);
+        localStorage.setItem('catGrass', catGrass);
+        displayCats.innerHTML = cats;
+        game();
+        alert('Game has been reset.')
+    }
+
+    displayCats.innerHTML = cats;
     document.getElementById('add').onclick = count;
-    document.getElementById('aboutNav').onclick = about;
-    document.getElementById('gameNav').onclick = game;
+    aboutNav.onclick = about;
+    gameNav.onclick = game;
+    resetNav.onclick = reset;
+    document.getElementById('resetGame').onclick = resetGame;
 });
